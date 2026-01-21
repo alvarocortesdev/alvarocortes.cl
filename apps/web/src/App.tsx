6a197fbd219dfaac38a9eb216a7574707e30068b
Alvaro Cortés <alvaro.cortes.dev@outlook.com>
2026-01-20 23:12:11 -0300

    feat(09-01): add /blog route with Layout wrapper
    - Import BlogPage component
    - Add Route for /blog path
    - BlogPage wrapped in Layout (has header + footer)

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom"
 import { Layout } from "./components/Layout"
 import { HomePage } from "./components/HomePage"
 import { PortfolioPage } from "./components/PortfolioPage"
+import { BlogPage } from "./components/BlogPage"
 
 export function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout><HomePage /></Layout>} />
       <Route path="/portfolio" element={<Layout><PortfolioPage /></Layout>} />
+      <Route path="/blog" element={<Layout><BlogPage /></Layout>} />
     </Routes>
   )
 }
